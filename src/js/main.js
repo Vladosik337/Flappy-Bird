971a8e6f1524c52c78635d55a15f60c056f6b7c4
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-09-28 00:51:36 +0300

    [FEAT] function  generateObstacle

--- src/js/main.js
+++ src/js/main.js
@@ -29,4 +29,30 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 
     document.addEventListener('click', jump);
+
+    function generateObstacle(){
+        let obstacleLeft = 500;
+        let randomHeight = Math.random() * 60;
+        let obstacleBottom = randomHeight;
+
+        const obstacle = document.createElement('div');
+        obstacle.classList.add('obstacle');
+        gameDisplay.appendChild(obstacle);
+        obstacle.style.left = obstacleLeft + 'px';
+        obstacle.style.bottom = obstacleBottom + 'px';
+
+        function moveObstacle(){
+            obstacleLeft -= 2;
+            obstacle.style.left = obstacleLeft + 'px'
+
+            if (obstacleLeft === -60){
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle);
+            }
+        }
+        let timerId = setInterval(moveObstacle, 20);
+        setTimeout(generateObstacle,3000);
+    }
+
+    generateObstacle();
 });
